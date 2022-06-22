a30e15628ca8609f4c37d3046b870ed0d4af419f
Alan Hernandez <alanhc93@gmail.com>
2022-06-21 19:45:16 -0500

    funciones condicionales y metodos de lista

--- basico-linux.py
+++ basico-linux.py
@@ -110,7 +110,7 @@ print(paises[-1])
 print(len(paises))
 #Esta es otra forma de crear una lista
 raza= list(("Chihuahua","baset hound","bulldog"))
-'''
+
 #----------Metodos de listas
 heroes =["Batman", "Superman", "Ironman"]
 villanos=["Joker", "Lex Luthor", "Dormamu"]
@@ -118,4 +118,84 @@ villanos.append("Darkseid")   #Agrega un villano mas
 #Une las listas
 heroes.extend(villanos)
 heroes.append("Capitan America")
+
+#Agregar un valor nuevo en otra parte de la lista
+heroes.insert(2, "Spiderman")
 print(heroes)
+#Elimina un valor de la lista
+heroes.remove("Dormamu")
+print(heroes)
+
+#Elimina una lista completa
+vacia= [1,2,3,4,5]
+vacia.clear()
+print(vacia)
+#Encuentra el indice de un valor de la lista
+nombres= ["Alan", "Maria", "Jesus", "Alberto", "Allison", "Alan"]
+print(nombres.index("Jesus"))
+#Cuantas veces aparece Alan en la lista
+print(nombres.count("Alan"))
+
+#Ordena una lista en orden ascendente 
+ascendente= [233, 34, 78, 984]
+ascendente.sort()
+print(ascendente)
+#Ahora ordenalos alreves
+ascendente.reverse()
+print(ascendente)
+#haz una copia de una lista
+copia= ["a" , "b", "c", "d"]
+copia2= copia.copy()
+print(copia2)
+#Borra el ultimo elemento de la lista
+copia2.pop()
+print(copia2)
+#Agrega z y borra c
+copia2.append("z")
+copia2.pop(2)#Con pop tambien se puede borrar mediante indices como remove
+print(copia2)
+
+#Tuples son como las listas pero inmutables osea que no se pueden modificar
+ejemplo= (1,2,True ,3,4, "prueba",5)
+print(ejemplo)
+#Obten el tipo de dato en el indice 2
+print(type(ejemplo[2]))
+#Crea un tuple con el constructor
+ejemplo2= tuple((5,9, False))
+print(ejemplo2)
+
+#---------------Functions-----------
+#Crea una calculadora con las operaciones basicas
+def calculadora():
+    num1 =int(input("Dime el primer numero: "))
+    num2=int(input("Dime el segundo numero: "))
+    print("La suma es: "+str(num1+num2))
+calculadora()
+
+#Cuando no sabes cuantos parametros tendras usar *
+def saludo(*nombres):
+    print("Bienvenido: "+nombres[1])
+saludo("Juan", "Jose", "Alan")
+
+#Calculadora dinamica usa return para devolver el valor
+def calcu(num1, num2):
+    res= print("La suma es: "+ str(num1+num2) + " la resta es: "+ str(num1-num2)+ " la multiplicacion es: "+ str(num1*num2)+ " la division es: "+ str(num1/num2))
+    return res
+num1 = int(input("Dime el primer numero: "))
+num2 = int(input("Dime el segundo numero: "))
+calcu(num1, num2)
+'''
+#-------------Condicionales------------
+#Crea una app que permita la entrada solo a mayores de 18 años en un horario de 5 de la noche a 12 de la mañana y las mujeres pagan la mitad a partir de las 9
+edad = int(input("¿Cual es tu edad?: "))
+hora = int(input("Horario de entrada?: "))
+genero = input("¿Cual es tu sexo?: ")
+def acceso():
+    if edad >= 18 and hora >= 17 and  hora < 24:
+        return print("Acceso permitido disfruta")
+    if edad >= 18 and hora >= 21 and hora < 24 and genero == "female":
+        return print("Acceso permitido y por ser chica pagas la mitad disfruta")
+    else:
+        return print("No tienes la edad necesaria para ingresar o el local esta cerrado sorry")
+acceso()
+
